22250ea0ff14b07841d787e06cebb99a543576bc
Carlos Bueno <carlos.bueno@csl.lu>
2024-09-24 10:46:03 +0200

    Initial Commit

---
+++ src/Orleans.Client.Multiple/OrleansClientHost.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Orleans.Client.Multiple;
+
+public sealed class OrleansClientHost
+{
+    public string Name { get; }
+    public IHost Host { get; }
+
+    public OrleansClientHost(string name, Action<IClientBuilder> configureClient)
+    {
+        Name = name;
+        Host = new HostBuilder()
+            .ConfigureServices(services =>
+            {
+                services.AddSingleton(this);
+            })
+            .UseOrleansClient(configureClient)
+            .Build();
+    }
+
+    public async Task StartAsync() => await Host.StartAsync();
+    public async Task StopAsync() => await Host.StopAsync();
+
+    public IClusterClient Client => Host.Services.GetRequiredService<IClusterClient>();
+}
---
+++ src/Orleans.Client.Multiple/OrleansClientManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orleans.Client.Multiple;
+
+public interface IOrleansClientManager
+{
+    void AddClient(string name, Action<IClientBuilder> configureClient);
+    Task StartAllAsync();
+    Task StopAllAsync();
+    IClusterClient GetClient(string name);
+}
+
+public sealed class OrleansClientManager : IOrleansClientManager
+{
+    private readonly Dictionary<string, OrleansClientHost> _clients = new();
+
+    public void AddClient(string name, Action<IClientBuilder> configureClient)
+    {
+        var clientHost = new OrleansClientHost(name, configureClient);
+        _clients[name] = clientHost;
+    }
+
+    public async Task StartAllAsync()
+    {
+        foreach (var clientHost in _clients.Values)
+        {
+            await clientHost.StartAsync();
+        }
+    }
+
+    public async Task StopAllAsync()
+    {
+        foreach (var clientHost in _clients.Values)
+        {
+            await clientHost.StopAsync();
+        }
+    }
+
+    public IClusterClient GetClient(string name)
+    {
+        if (_clients.TryGetValue(name, out var clientHost))
+        {
+            return clientHost.Client;
+        }
+        throw new KeyNotFoundException($"Cliente Orleans com o nome '{name}' não foi encontrado.");
+    }
+}
+
